dd454e28757fa0d7e86e6d8e82e0064b49b66d6f
Chengyu Sun <csun@calstatela.edu>
2024-08-30 11:34:48 -0700

    Added email alert on exception and shutdown

--- AliceMailService/Worker.cs
+++ AliceMailService/Worker.cs
@@ -23,6 +23,8 @@ public class EmailSettings
     public bool RequireAuthentication { get; set; } = false;
     public string Username { get; set; } = "cysun@localhost.localdomain";
     public string Password { get; set; } = "abcd";
+    public string AlertSender { get; set; }
+    public string AlertRecipient { get; set; }
 }
 
 public class Worker : BackgroundService
@@ -52,31 +54,40 @@ public class Worker : BackgroundService
             UserName = _mqSettings.UserName,
             Password = _mqSettings.Password
         };
-        _connection = _factory.CreateConnection();
-        _logger.LogInformation("Connected to RabbitMQ server at {host}", _mqSettings.HostName);
 
-        _channel = _connection.CreateModel();
-        _channel.QueueDeclare(queue: _mqSettings.QueueName,
-            durable: true,
-            exclusive: false,
-            autoDelete: false,
-            arguments: null);
-        _logger.LogInformation("Queue {queue} declared", _mqSettings.QueueName);
-
-        _consumer = new EventingBasicConsumer(_channel);
-        _consumer.Received += async (model, ea) =>
+        try
         {
-            var messages = MessagePackSerializer.Deserialize<List<byte[]>>(ea.Body.ToArray())
-                .Select(bytes =>
-                {
-                    using var stream = new MemoryStream(bytes);
-                    return MimeMessage.Load(stream);
-                }).ToList();
-            await SendEmailAsync(messages);
-            _channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
-        };
-        _channel.BasicConsume(queue: _mqSettings.QueueName, autoAck: false, consumer: _consumer);
-        _logger.LogInformation("Consumer listens to queue {queue}", _mqSettings.QueueName);
+            _connection = _factory.CreateConnection();
+            _logger.LogInformation("Connected to RabbitMQ server at {host}", _mqSettings.HostName);
+
+            _channel = _connection.CreateModel();
+            _channel.QueueDeclare(queue: _mqSettings.QueueName,
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+            _logger.LogInformation("Queue {queue} declared", _mqSettings.QueueName);
+
+            _consumer = new EventingBasicConsumer(_channel);
+            _consumer.Received += async (model, ea) =>
+            {
+                var messages = MessagePackSerializer.Deserialize<List<byte[]>>(ea.Body.ToArray())
+                    .Select(bytes =>
+                    {
+                        using var stream = new MemoryStream(bytes);
+                        return MimeMessage.Load(stream);
+                    }).ToList();
+                await SendEmailAsync(messages);
+                _channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+            };
+            _channel.BasicConsume(queue: _mqSettings.QueueName, autoAck: false, consumer: _consumer);
+            _logger.LogInformation("Consumer listens to queue {queue}", _mqSettings.QueueName);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Failed to connect to RabbitMQ server at {host}", _mqSettings.HostName);
+            SendAlert("Failed to Connect to RabbitMQ Server", ex.Message);
+        }
 
         return Task.CompletedTask;
     }
@@ -118,11 +129,61 @@ public class Worker : BackgroundService
         await client.DisconnectAsync(true);
     }
 
+    // This is basically a synchronous version of SendEmail. There should be no need for this method since we should
+    // be able to use SendMailAsync, but the problem is that C# has some weird rules about await an async method in a
+    // catch block, which is where I want to send an alert email. Technically we can await an async method in a catch
+    // block since C# 6, but it would require changing the return type of the method from Task to "async Task<Task>".
+    // I've yet to fully understood this, so the current workaround is to use this synchronous method.
+    private void SendAlert(string subject, string content)
+    {
+        var message = new MimeMessage();
+        message.From.Add(MailboxAddress.Parse(_emailSettings.AlertSender));
+        message.To.Add(MailboxAddress.Parse(_emailSettings.AlertRecipient));
+        message.Subject = subject;
+        message.Body = new TextPart("plain")
+        {
+            Text = content
+        };
+
+        using var client = new SmtpClient();
+
+        try
+        {
+            if (_emailSettings.RequireAuthentication)
+            {
+                client.Connect(_emailSettings.Host, _emailSettings.Port, SecureSocketOptions.StartTls);
+                client.Authenticate(_emailSettings.Username, _emailSettings.Password);
+            }
+            else // for testing with local SMTP server
+            {
+                client.Connect(_emailSettings.Host, _emailSettings.Port, SecureSocketOptions.None);
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Failed to connect to SMTP server");
+        }
+
+        try
+        {
+            client.SendAsync(message);
+            _logger.LogInformation("Message [{subject}] sent to {receipient}", message.Subject, message.To);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Failed to send message [{subject}] sent to {receipient}", message.Subject, message.To);
+        }
+
+        client.Disconnect(true);
+    }
+
     public override Task StopAsync(CancellationToken cancellationToken)
     {
         _channel.Close();
         _connection.Close();
         _logger.LogInformation("Channel and connection closed");
+
+        SendAlert("AliceMailService Stopped", "The service has been stopped.");
 
         return base.StopAsync(cancellationToken);
     }
